14e72cd06afb6755e2b642cc23150bdf1ab86953
Vilius <30282958+viliusddd@users.noreply.github.com>
2018-06-07 07:44:13 +0100

    Add files via upload

---
+++ obj-parser.py
@@ -0,0 +1,67 @@
+from os import listdir
+from os.path import isfile, isdir, join, splitext, basename
+from sys import argv
+from zipfile import ZipFile
+import csv
+import json
+import re
+
+
+class obj(object):
+    def __init__(self, path):
+        self.path = path
+
+    def is_maya(self):
+        filesObj = []
+        filesAll = []
+
+        if isdir(self.path):
+            for root, dirs, files in walk(self.path):
+                for item in files:
+                    filesAll.append(join(root, item))
+            for item in filesAll:
+                if item.endswith('.obj'):
+                    filesObj.append(item)
+            return list(filesObj)
+
+        elif isfile(self.path):
+            if self.path.endswith('.obj'):
+                return self.path
+
+    def tokenize(self):
+        pass
+# Token Specification
+GEO = re.compile(r'(?P<GEO>g ([a-zA-Z_0-9]*))')
+LIB = re.compile(r'(?P<LIB>(?:(?:mtllib)|(?:adobemdllib)) ([a-zA-Z0-9_]*.(?:mtl)|(?:mdl)))')
+USE = re.compile(r'(?P<USE>usemtl ([a-zA-Z_0-9]*))')
+TRI = re.compile(r'(?P<TRI>f ((?:[0-9/]*(?: |)){3}(?!...)))')
+
+#geo_pat = re.compile(GEO) # geometry name(s)
+#lib_pat = re.compile(TRI) # mtl/mdl file name and or if it exists
+#use_pat = re.compile(USE) # material name(s)
+#tri_pat = re.compile(TRI) # triangle count
+
+tris = int(0) #green
+quads = int(0) # green
+fiveGons = int(0) # yellow
+nGons = int(0) # red
+mtl_name = []
+with open(argv[1], "rt") as f:
+    for line in f:
+        f.readline().rstrip()
+        #print(line.rstrip())
+        u = USE.scanner(line.rstrip()).match()
+        t = TRI.scanner(line.rstrip()).match()
+        l = LIB.scanner(line.rstrip()).match()
+        g = GEO.scanner(line.rstrip()).match()
+        if u:
+            print(u)
+            #mtl_name.append(u.group(0))
+        elif t:
+            tris += 1
+        elif g:
+            print(g.group(2))
+        elif l:
+            mtl_name.append(l.group(2))
+    print(mtl_name)
+    print(tris)
